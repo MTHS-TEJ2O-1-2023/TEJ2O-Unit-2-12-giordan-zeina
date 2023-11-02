21bea5afc5044f83dbf214208878febc6b7392a3
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-11-02 13:23:54 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -2,12 +2,12 @@
  *
  * Created by: Giordan Zeina
  * Created on: Oct 2023
- * This program uses the distance sensor to turn all the neopixels to red, if the distance is < 10 cm 
+ * This program uses the distance sensor to turn all the neopixels to red, if the distance is < 10 cm
  * and also turn the neopixels to green, if the distance is >= 10 cm
 */
 
 // variables
-let distance:number = 0
+let distance: number = 0
 let neopixelStrip: neopixel.Strip = null
 
 // cleanup
@@ -32,7 +32,6 @@ input.onButtonPressed(Button.A, function () {
     neopixelStrip.show()
     basic.showIcon(IconNames.Happy)
   } else {
-
     // if the distance is > 10 turns the neopixels Green
     neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
     neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
